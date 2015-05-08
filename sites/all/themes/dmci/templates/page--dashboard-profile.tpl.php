5bf28b84481b69e9d35f690cb4049607e989eb64
Gerald Villorente <geraldvillorente@gmail.com>
2015-05-08 08:51:43 +0800

    Updated profile tpl

--- sites/all/themes/dmci/templates/page--dashboard-profile.tpl.php
+++ sites/all/themes/dmci/templates/page--dashboard-profile.tpl.php
@@ -1,26 +1,56 @@
-<div class="contacts row">
+<div class="contacts row" ng-controller="userController">
   <div class="large-10 columns large-centered">
-    <div class="contact-info">
+    <div class="contact-info" ng-repeat="profile in user.response track by profile.user.uid">
       <img src="<?php print $logo; ?>" class="profile-logo">
       <div class="large-3 columns">
         <center>
-          <img src="<?php print $base_url_default_files . "unknown.jpg"; ?>" />
-          <div><button class="back"> EDIT PROFILE </button></div>
+          <img src="{{profile.user.image}}" />
+          <div><a href="/user/{{profile.user.uid}}/edit/main" class="button back"> EDIT PROFILE </a></div>
         </center>
       </div>
       <div class="large-9 columns">
         <p class="title"> Contact Information </p>
-        <div class="field row"> <div class="large-3 columns"> Name: </div>  <div class="large-9 columns">Maynard Cabalitan</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Email: </div>  <div class="large-9 columns">maynard.cabalitan@gmail.com</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Contact: </div>  <div class="large-9 columns">+63915-132-1006</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Website: </div>  <div class="large-9 columns">http://salesph.com</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Birthday: </div>  <div class="large-9 columns">June 8, 1975</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Position: </div>  <div class="large-9 columns">Sales Broker</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Department: </div>  <div class="large-9 columns">Sales Department</div> </div>
+        <div class="field row">
+          <div class="large-3 columns"> Name: </div>
+          <div class="large-9 columns">{{profile.user.name}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Email: </div>
+          <div class="large-9 columns">{{profile.user.email}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Contact: </div>
+          <div class="large-9 columns">{{profile.user.contact}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Website: </div>
+          <div class="large-9 columns">{{profile.user.website}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Birthday: </div>
+          <div class="large-9 columns">{{profile.user.birthday}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Position: </div>
+          <div class="large-9 columns">{{profile.user.position}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Department: </div>
+          <div class="large-9 columns">{{profile.user.department}}</div>
+        </div>
         <p class="title"> Additional Information </p>
-        <div class="field row"> <div class="large-3 columns"> City: </div>  <div class="large-9 columns">Manila</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Skills: </div>  <div class="large-9 columns">Eating, Sleeping, Reading, Coding, Playing, Etc</div> </div>
-        <div class="field row"> <div class="large-3 columns"> Interest: </div>  <div class="large-9 columns">Programming, Music, Dota 2</div> </div>
+        <div class="field row">
+          <div class="large-3 columns"> City: </div>
+          <div class="large-9 columns">{{profile.user.city}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Skills: </div>
+          <div class="large-9 columns">{{profile.user.skills}}</div>
+        </div>
+        <div class="field row">
+          <div class="large-3 columns"> Interest: </div>
+          <div class="large-9 columns">{{profile.user.interests}}</div>
+        </div>
       </div>
       <div class="clear-both"> </div>
     </div>
