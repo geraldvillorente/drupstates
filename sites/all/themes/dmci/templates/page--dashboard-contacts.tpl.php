10e216796be835a834c9830b85387ed017f8472c
Gerald Villorente <geraldvillorente@gmail.com>
2015-05-08 08:22:45 +0800

    Update contacts tpl

--- sites/all/themes/dmci/templates/page--dashboard-contacts.tpl.php
+++ sites/all/themes/dmci/templates/page--dashboard-contacts.tpl.php
@@ -1,76 +1,10 @@
 <div class="contacts row">
-  <div class="letters">
-    <ul>
-      <li>a</li><li>b</li><li>c</li><li>d</li><li>e</li><li>f</li><li>g</li><li>h</li><li>i</li><li>j</li><li>k</li><li>l</li><li>m</li><li>n</li>
-      <li>o</li><li>p</li><li>q</li><li>r</li><li>s</li><li>t</li><li>u</li><li>v</li><li>w</li><li>x</li><li>y</li><li>z</li>
-    </ul>
-  </div>
   <div class="clear-both"></div>
 
-  <a href="single-contact">
   <div class="contact-container">
-  <div class="contact">
-    <div class="large-5 small-12 columns">
-      <div class="large-4 columns"> <img src="<?php print $base_url_default_files . "unknown.jpg"; ?>" /> </div>
-      <div class="large-8 columns">
-        <div class="field row"> <div class="large-3 columns"> Name: </div>  <div class="large-9 columns">Maynard Cabalitan</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Contact: </div>  <div class="large-9 columns">+63915-132-1006</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Email: </div>  <div class="large-9 columns">maynard.cabalitan@gmail.com</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Address: </div>  <div class="large-9 columns">Schouhauser Allee 167c 10435 Berlin Germany</div>  </div>
-      </div>
-    </div>
+    <?php $view = views_get_view('contacts'); ?>
+    <?php print $view->render('block_1'); ?>
   </div>
-  </a>
-
-  <div class="contact">
-    <div class="large-5 small-12 columns">
-      <div class="large-4 columns"> <img src="<?php print $base_url_default_files . "unknown.jpg"; ?>" /> </div>
-      <div class="large-8 columns">
-        <div class="field row"> <div class="large-3 columns"> Name: </div>  <div class="large-9 columns">Maynard Cabalitan</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Contact: </div>  <div class="large-9 columns">+63915-132-1006</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Email: </div>  <div class="large-9 columns">maynard.cabalitan@gmail.com</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Address: </div>  <div class="large-9 columns">Schouhauser Allee 167c 10435 Berlin Germany</div>  </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="contact">
-    <div class="large-5 small-12 columns">
-      <div class="large-4 columns"> <img src="<?php print $base_url_default_files . "unknown.jpg"; ?>" /> </div>
-      <div class="large-8 columns">
-        <div class="field row"> <div class="large-3 columns"> Name: </div>  <div class="large-9 columns">Maynard Cabalitan</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Contact: </div>  <div class="large-9 columns">+63915-132-1006</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Email: </div>  <div class="large-9 columns">maynard.cabalitan@gmail.com</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Address: </div>  <div class="large-9 columns">Schouhauser Allee 167c 10435 Berlin Germany</div>  </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="contact">
-    <div class="large-5 small-12 columns">
-      <div class="large-4 columns"> <img src="<?php print $base_url_default_files . "unknown.jpg"; ?>" /> </div>
-      <div class="large-8 columns">
-        <div class="field row"> <div class="large-3 columns"> Name: </div>  <div class="large-9 columns">Maynard Cabalitan</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Contact: </div>  <div class="large-9 columns">+63915-132-1006</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Email: </div>  <div class="large-9 columns">maynard.cabalitan@gmail.com</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Address: </div>  <div class="large-9 columns">Schouhauser Allee 167c 10435 Berlin Germany</div>  </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="contact">
-    <div class="large-5 small-12 columns">
-      <div class="large-4 columns"> <img src="<?php print $base_url_default_files . "unknown.jpg"; ?>" /> </div>
-      <div class="large-8 columns">
-        <div class="field row"> <div class="large-3 columns"> Name: </div>  <div class="large-9 columns">Maynard Cabalitan</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Contact: </div>  <div class="large-9 columns">+63915-132-1006</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Email: </div>  <div class="large-9 columns">maynard.cabalitan@gmail.com</div>  </div>
-        <div class="field row"> <div class="large-3 columns"> Address: </div>  <div class="large-9 columns">Schouhauser Allee 167c 10435 Berlin Germany</div>  </div>
-      </div>
-    </div>
-  </div>
-  </div>
-
 </div>
 
 <style>
