603b485e49f08ebe56537edd2cdfdced9f3df493
Gerald Villorente <geraldvillorente@gmail.com>
2015-05-24 13:03:49 +0800

    Made th reservation dashboard to utilize Angular rendering instead of default Drupal render method

--- sites/all/themes/dmci/templates/page--dashboard-reservation.tpl.php
+++ sites/all/themes/dmci/templates/page--dashboard-reservation.tpl.php
@@ -1,46 +1,26 @@
-<div class="reservation-list large-11 columns large-centered">
+<div class="reservation-list large-11 columns large-centered" ng-controller="reservationListController">
   <table width="100%">
     <thead>
       <tr>
         <th>Name</th>
         <th>Unit</th>
         <th>Development</th>
-        <th>Unit Type</th>
         <th>Reservation Date</th>
         <th>Reserved By</th>
         <th>Status</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Gilda Glatz</td>
-        <td>101</td>
-        <td>The Birchwood</td>
-        <td>2 - Bedroom</td>
-        <td>4 / 1 / 2014</td>
-        <td>Zena Bumgardner</td>
-        <td>APPROVED</td>
+
+      <tr ng-repeat="list in reservationList.response">
+        <td>{{list.node.firstName}} {{list.node.lastName}}</td>
+        <td>{{list.node.unit}}</td>
+        <td>{{list.node.development}}</td>
+        <td>{{list.node.date}}</td>
+        <td>{{list.node.name}}</td>
+        <td>{{list.node.status}}</td>
       </tr>
 
-      <tr>
-        <td>Cyndia Bengston</td>
-        <td>102</td>
-        <td>The Birchwood</td>
-        <td>3 - Bedroom</td>
-        <td>4 / 2 / 2014</td>
-        <td>Zella Schuller</td>
-        <td>DENIED</td>
-      </tr>
-
-      <tr>
-        <td>Gilda Glatz</td>
-        <td>103</td>
-        <td>The Birchwood</td>
-        <td>3 - Bedroom</td>
-        <td>4 / 3 / 2014</td>
-        <td>Zena Bumgardner</td>
-        <td>SOLD</td>
-      </tr>
     </tbody>
   </table>
 </div>
